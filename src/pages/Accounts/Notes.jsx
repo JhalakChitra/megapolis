0a46a03b00c8f6641154d10f7dbf063ca1e44552
jhalak <jhalakchitra2000@gmail.com>
2025-11-06 17:00:54 +0530

    Updated project files

--- src/pages/Accounts/Notes.jsx
+++ src/pages/Accounts/Notes.jsx
@@ -1,7 +1,13 @@
+import ClientDocuments from "../../components/Accounts/Notes/ClientDocuments";
+import ClientNotes from "../../components/Accounts/Notes/ClientNotes";
+
 const Notes=()=>{
-    return(<>
-    Notes
-    </>)
+    return(
+    <div className="space-y-6 bg-gray-50 p-4 sm:p-6 min-h-screen">
+      <ClientNotes />
+      <ClientDocuments />
+    </div>
+    )
 }
 
 export default Notes;
