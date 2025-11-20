70062ad77e6ec499d0c07a5e25db50af25d0cdde
jhalak <jhalakchitra2000@gmail.com>
2025-11-20 11:10:06 +0530

    Updated project files

--- src/components/Accounts/SignInForm.jsx
+++ src/components/Accounts/SignInForm.jsx
@@ -1,17 +1,51 @@
-// import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const SignInForm = () => {
+      
+    const navigate = useNavigate();
+
+     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await axios.post("http://localhost:8000/auth/login", {
+            email,
+            password
+        });
+
+        // Save token
+        localStorage.setItem("token", response.data.access_token);
+
+        // Navigate to next page
+        navigate("/CreateOrganization");
+
+    } catch (error) {
+        alert("Invalid email or password");
+    }
+};
+
+
+
+
     return (
         <div className="bg-[#F9F9F9] p-5 border border-gray-200 rounded-xl">
             <h2 className="text-3xl font-bold mb-2">Sign In</h2>
             <p className="text-gray-500 mb-6">Enter your email and password to sign in!</p>
 
-            <form>
+            <form  onSubmit={handleSubmit}>
                 <div className="mb-4">
                     <label className="block text-sm font-medium mb-1">Email address<span className="text-red-500">*</span></label>
                     <input
                         type="email"
+                           value={email}
+                         onChange={(e) => setEmail(e.target.value)}
                         placeholder="Enter your email"
                         className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
                     />
@@ -20,7 +54,9 @@ const SignInForm = () => {
                 <div className="mb-4">
                     <label className="block text-sm font-medium mb-1">Password<span className="text-red-500">*</span></label>
                     <input
-                        type="password"
+                         type="password"
+                            value={password}
+                         onChange={(e) => setPassword(e.target.value)}
                         className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
                     />
                 </div>
